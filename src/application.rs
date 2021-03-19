9fa105e5b3e750db94dcd9e7c5b303f935558275
Jin <zjhken01@163.com>
2021-03-20 06:52:24 +0800

    it works

--- src/application.rs
+++ src/application.rs
@@ -1,1 +1,51 @@
 #![allow(non_snake_case)]
+
+use anyhow::Result;
+use anyhow::Error;
+use smol::future::Future;
+use std::pin::Pin;
+use futures::future::BoxFuture;
+
+pub struct AsyncFnPtr<R> {
+	func: Box<dyn Fn(i32, i32) -> BoxFuture<'static, R> + Send + 'static>
+}
+
+impl<R> AsyncFnPtr<R> {
+	pub fn new<F>(f: fn(i32, i32) -> F) -> AsyncFnPtr<F::Output>
+		where F: Future<Output=R> + Send + 'static {
+		AsyncFnPtr {
+			func: Box::new(move |a, b| Box::pin(f(a, b))),
+		}
+	}
+	// async fn run(&self) -> R { (self.func)().await }
+}
+
+
+pub struct App {
+	handlers: Vec<Box<dyn Future<Output=bool>>>,
+}
+
+impl App {
+	pub async fn setHandler<Fut>(mut handlers: Vec<AsyncFnPtr<bool>>, f: fn(i32, i32) -> Fut) -> Result<bool, Error>
+		where
+				Fut: Future<Output=bool> + Send + 'static,
+	{
+		handlers.push(AsyncFnPtr::new(f));
+		handlers
+				.iter()
+				.enumerate()
+				.map(|(index, asyncFnPtr)| {
+					smol::block_on(async {
+						(asyncFnPtr.func)(index as i32, 1i32).await
+						// asyncFnPtr(index as i32, 1i32).await
+					})
+				})
+				.collect::<Vec<bool>>();
+		// Ok(f(1, 2).await)
+		Ok(true)
+	}
+}
+
+pub fn next(){
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
+mod application;
+mod context;
+mod request;
+mod response;
+
+
 #[cfg(test)]
 mod tests {
+	use crate::application::{App, AsyncFnPtr};
+	use crate::haha;
+
 	#[test]
 	fn it_works() {
-		
+		let _ = smol::block_on(async {
+			let v = vec![AsyncFnPtr::new(hehe)];
+			App::setHandler(v, haha).await
+		});
+	}
+
+	async fn hehe(a: i32, b: i32) -> bool {
+		return a + b > 0i32;
 	}
 }
 
 
 
+async fn haha(a: i32, b: i32)-> bool{
+	println!("haha {}", a + b);
+	return true;
+}
