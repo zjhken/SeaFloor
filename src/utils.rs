19bffb5052f1723d585d2b471e7a9ba21bfae892
Jin <zjhken01@163.com>
2021-03-28 09:56:43 +0800

    abstract utils

---
+++ src/utils.rs
@@ -0,0 +1,24 @@
+#![allow(non_snake_case)]
+
+use futures::{future::BoxFuture, Future};
+use std::pin::Pin;
+
+use crate::context::Context;
+
+pub struct AsyncFnPtr<R> {
+	pub func: Box<dyn Fn(Context) -> BoxFuture<'static, R> + Send + 'static + Sync>,
+}
+
+impl<R> AsyncFnPtr<R> {
+	pub fn new<F>(f: fn(Context) -> F) -> AsyncFnPtr<F::Output>
+		where
+				F: Future<Output=R> + Send + 'static,
+	{
+		AsyncFnPtr {
+			func: Box::new(move |a| Box::pin(f(a))),
+		}
+	}
+	pub async fn run(&self, context: Context) -> R {
+		return (self.func)(context).await
+	}
+}
